1adadee2a60e52c76b207a081dcbc52f949c5037
alice martins <alicesilvah29@gmail.com>
2025-09-14 13:52:12 -0300

    first commit

---
+++ magica.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls')
+
+print('Olhe as cartas abaixo e  escolha uma:')
+print('---')
+print('Q♥️   K♠️   J♣️  Q♦️  J♥️   K♣️')
+input()
+
+os.system('cls')
+print('A carta que você pensou...')
+input()
+
+print()
+print('Desapareceu!')
+print()
+print('K♣️   J🖤  Q♦️  J♥️   K♣️')
+print()
