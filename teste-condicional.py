9a96f4034d19045c3189451832b7c34827345220
alice martins <alicesilvah29@gmail.com>
2025-09-14 14:42:18 -0300

    primeiro commit

---
+++ teste-condicional.py
@@ -0,0 +1,10 @@
+print('Bem-vindo ao site do DETRAN')
+print('Esse é o novo teste para definir se você está apto a tirar a CNH ou não')
+print()
+idade = int(input('Digite sua idade: '))
+
+if idade >=18:
+    print('Pode dirigir!')
+else:
+    print('Não pode dirigir')
+print('Tente novamente daqui uns anos, obrigada ')     
